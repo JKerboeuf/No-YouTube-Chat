7a111f8f789f660e7b8f5a4c26bb7028af83bb44
Julien <42395792+PsychoPatate@users.noreply.github.com>
2020-11-28 05:32:21 +0100

    update infos

--- No-YT-Chat.js
+++ No-YT-Chat.js
@@ -4,12 +4,12 @@
 // @author		PsychoPatate
 // @namespace	https://github.com/PsychoPatate
 // @version		1.0
-// @source		https://github.com/PsychoPatate/BLABLAPROJET
-// @iconURL		https://github.com/PsychoPatate/BLABLAPROJET/icon.png
-// @icon64URL	https://github.com/PsychoPatate/BLABLAPROJET/icon.png
-// @updateURL	https://github.com/PsychoPatate/BLABLAPROJET/src.js
-// @downloadURL	https://github.com/PsychoPatate/BLABLAPROJET/src.js
-// @supportURL	https://github.com/PsychoPatate/BLABLAPROJET/issues
+// @source		https://github.com/PsychoPatate/No-YT-Chat
+// @iconURL		https://raw.githubusercontent.com/PsychoPatate/No-YT-Chat/main/icon128.png
+// @icon64URL	https://raw.githubusercontent.com/PsychoPatate/No-YT-Chat/main/icon64.png
+// @updateURL	https://raw.githubusercontent.com/PsychoPatate/No-YT-Chat/main/No-YT-Chat.js
+// @downloadURL	https://raw.githubusercontent.com/PsychoPatate/No-YT-Chat/main/No-YT-Chat.js
+// @supportURL	https://github.com/PsychoPatate/No-YT-Chat/issues
 // @require		https://code.jquery.com/jquery-3.5.1.min.js
 // @match		https://www.youtube.com/watch*
 // ==/UserScript==
